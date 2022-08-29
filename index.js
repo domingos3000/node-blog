421ad515f64ebf187b6df771f933070bfa4b3112
Domingos Nkula Pedro <domingosnkulajw@gmail.com>
2022-08-29 03:58:08 +0100

    Update server

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('X-Content-Type-Options', 'nosniff');
 
     if(req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
